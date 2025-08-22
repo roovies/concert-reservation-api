c46fa5ba76117e4607e8198e96885adb3d3b0cad
jihwanlee <roovies77@gmail.com>
2025-08-22 17:49:26 +0900

    init:초기 프로젝트 추가

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.1"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+fun getGitHash(): String {
+	return providers.exec {
+		commandLine("git", "rev-parse", "--short", "HEAD")
+	}.standardOutput.asText.get().trim()
+}
+
+group = "com.roovies"
+version = getGitHash()
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+	}
+}
+
+dependencies {
+    // Spring
+	implementation("org.springframework.boot:spring-boot-starter-actuator") // 애플리케이션의 운영 및 상태를 모니터링하고 관리하기 위한 다양한 엔드포인트를 제공하는 스타터(starter) 라이브러리
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // DB
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:mysql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	systemProperty("user.timezone", "UTC")
+}
