b96dfb033c26cad55be9abe103041262ffe017ab
roovies <roovies77@gmail.com>
2025-09-12 12:46:11 +0900

    refactor: User JPA Entity 설계 및 패키지 구조 개선

--- src/main/java/com/roovies/concertreservation/users/infra/adapter/out/persistence/UserRepositoryAdapter.java
+++ src/main/java/com/roovies/concertreservation/users/infra/adapter/out/persistence/UserRepositoryAdapter.java
@@ -1,9 +1,9 @@
 package com.roovies.concertreservation.users.infra.adapter.out.persistence;
 
-import com.roovies.concertreservation.users.application.port.out.UserRepository;
+import com.roovies.concertreservation.users.application.port.out.UserRepositoryPort;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class UserRepositoryImpl implements UserRepository {
+public class UserRepositoryPortImpl implements UserRepositoryPort {
     private final UserJpaRepository userJpaRepository;
 }
