62bfdda891e1c6ec4ecdadbd913edc65112fbe11
jihwanlee <roovies77@gmail.com>
2025-11-28 22:55:32 +0900

    refactor(ranking): 주간 랭킹 업데이트 cron 매주 월요일 자정으로 수정

--- src/main/java/com/roovies/concertreservation/ranking/infra/adapter/in/scheduler/WeeklyRankingScheduler.java
+++ src/main/java/com/roovies/concertreservation/ranking/infra/adapter/in/scheduler/WeeklyRankingScheduler.java
@@ -25,10 +25,10 @@ public class WeeklyRankingScheduler {
     /**
      * 주간 랭킹 업데이트 스케줄러.
      * <p>
-     * 매시간마다 주간 랭킹을 재계산하여 Redis에 캐싱한다.
-     * ShedLock을 사용하여 분산 환경에서 중복 실행을 방지한다.
+     * 매주 월요일 0시(자정)에 주간 랭킹을 재계산하여 Redis에 캐싱한다.
+     * ShedLock을 사용해 분산 환경에서 중복 실행을 방지한다.
      */
-    @Scheduled(cron = "0 0 * * * *")  // 매시간 정각
+    @Scheduled(cron = "0 0 0 * * MON")  // 매주 월요일 00:00
     @SchedulerLock(name = "updateWeeklyRanking", lockAtMostFor = "10m", lockAtLeastFor = "1m")
     public void updateWeeklyRanking() {
         log.info("=== 주간 랭킹 업데이트 시작 ===");
